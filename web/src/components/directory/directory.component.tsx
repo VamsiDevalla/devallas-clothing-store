337ddad0a637b88d03ddb28ed46487bc4661584a
vamsidevalla <vammichanti40312@gmail.com>
2021-11-04 12:13:10 -0400

    backing up inital db items

--- web/src/components/directory/directory.component.tsx
+++ web/src/components/directory/directory.component.tsx
@@ -10,45 +10,11 @@ type MenuItemModel = {
   size?: 'large' | 'sm' | 'xl' | 'md';
 };
 
-const menuItemList: MenuItemModel[] = [
-  {
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    id: 1,
-    linkUrl: 'shop/hats',
-  },
-  {
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    id: 2,
-    linkUrl: 'shop/jackets',
-  },
-  {
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    id: 3,
-    linkUrl: 'shop/sneakers',
-  },
-  {
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    size: 'large',
-    id: 4,
-    linkUrl: 'shop/womens',
-  },
-  {
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    size: 'large',
-    id: 5,
-    linkUrl: 'shop/mens',
-  },
-];
 class Directory extends Component<Record<string, unknown>, { menuItems: MenuItemModel[] }> {
   constructor(properties: PropsWithChildren<Record<string, unknown>>) {
     super(properties);
     this.state = {
-      menuItems: menuItemList,
+      menuItems: [],
     };
   }
 
